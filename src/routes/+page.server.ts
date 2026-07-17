06877af922e3717da3d44626b493487c25c00344
Kiss Benedek Máté <tasztalos69@gmail.com>
2026-07-17 20:43:07 +0200

    Disable prerender, update action versions

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 import { env } from '$env/dynamic/private';
 import { list } from '$lib/server/cache.js';
 
+export const prerender = false;
+
 export const load = () => ({
 	username: env.UGYFELKAPU_USERNAME ?? '',
 	recent: list()
